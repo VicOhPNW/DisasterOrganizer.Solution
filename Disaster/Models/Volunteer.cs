348506cebc02ed3192d616e79b70328548c84c19
Kenneth Du <kennethdu3@gmail.com>
2018-10-01 08:54:23 -0700

    add placeholder methods for Volunteers.cs file

--- Disaster/Models/Volunteer.cs
+++ Disaster/Models/Volunteer.cs
@@ -22,6 +22,41 @@ namespace Disaster.Models
         {
             return _id;
         }
+        public static void DeleteAll()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        public void Edit(string newName)
+        {
+
+        }
+        public void Save()
+        {
+
+        }
+        public static List<Volunteer> GetAll()
+        {
+            List<Volunteer> allVolunteers = new List<Volunteer> { };
+            return allVolunteers;
+        }
+        public static Volunteer Find(int id)
+        {
+            Volunteer newVolunteer = new Volunteer(volunteerName, volunteerId);
+            return newVolunteer;
+        }
+        public List<Disaster> GetDisaster()
+        {
+            List<Disaster> VolunteerDisaster = new List<Disaster>{};
+            return VolunteerDisaster;
+        }
+        public void AddDisaster(Disaster newDisaster)
+        {
+
+        }
 
     }
 }
